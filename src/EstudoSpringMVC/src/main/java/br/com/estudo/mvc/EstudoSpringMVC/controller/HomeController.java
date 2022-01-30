9e9ce2a01c6393b4467689cf6e6965e5fda2fd2e
GUSTAVO DE ARAUJO SILVA <gustavodzn@Live.com>
2022-01-29 21:55:48 -0300

    .

--- src/EstudoSpringMVC/src/main/java/br/com/estudo/mvc/EstudoSpringMVC/controller/HomeController.java
+++ src/EstudoSpringMVC/src/main/java/br/com/estudo/mvc/EstudoSpringMVC/controller/HomeController.java
@@ -1,15 +1,29 @@
 package br.com.estudo.mvc.EstudoSpringMVC.controller;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.estudo.mvc.EstudoSpringMVC.model.ProductOrder;
+import java.util.List;
 
 @Controller
 public class HomeController {
-	@GetMapping("/hello")
-	public String hello( HttpServletRequest request) {
-		request.setAttribute("nome", "Teste");
-		return "hello";
+	@GetMapping("/home")
+	public String home(Model model) {
+		ProductOrder productOrder = new ProductOrder();
+		productOrder.setProductName("Notebook Gamer Acer Nitro 5");
+		productOrder.setProductDescription("Notebook da Acer");
+		productOrder.setProductImage("https://m.media-amazon.com/images/I/81lO5pdJAbL._AC_SX450_.jpg");
+		productOrder.setProductURL("https://www.amazon.com.br/dp/B09JVHNF4Q/ref=s9_acsd_hps_bw_c2_x_0_t?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-8&pf_rd_r=MTNPMJ24XE4JJ1B72PPW&pf_rd_t=101&pf_rd_p=d151abd6-f44f-49ff-835f-36eb0c8da138&pf_rd_i=17923699011");
+		
+		List<ProductOrder> listProducts = Arrays.asList(productOrder);
+		model.addAttribute("products", listProducts);
+		
+		return "home";
 	}
 }
---
+++ src/EstudoSpringMVC/src/main/java/br/com/estudo/mvc/EstudoSpringMVC/model/ProductOrder.java
@@ -0,0 +1,90 @@
+package br.com.estudo.mvc.EstudoSpringMVC.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ProductOrder {
+	private String productName;
+	private BigDecimal amountPaid;
+	private LocalDate deliveryDate;
+	private String productURL;
+	private String productImage;
+	private String productDescription;
+	
+	
+	/**
+	 * @return the productImage
+	 */
+	public String getProductImage() {
+		return productImage;
+	}
+	/**
+	 * @param productImage the productImage to set
+	 */
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * @return the amountPaid
+	 */
+	public BigDecimal getAmountPaid() {
+		return amountPaid;
+	}
+	/**
+	 * @param amountPaid the amountPaid to set
+	 */
+	public void setAmountPaid(BigDecimal amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+	/**
+	 * @return the deliveryDate
+	 */
+	public LocalDate getDeliveryDate() {
+		return deliveryDate;
+	}
+	/**
+	 * @param deliveryDate the deliveryDate to set
+	 */
+	public void setDeliveryDate(LocalDate deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+	/**
+	 * @return the productURL
+	 */
+	public String getProductURL() {
+		return productURL;
+	}
+	/**
+	 * @param productURL the productURL to set
+	 */
+	public void setProductURL(String productURL) {
+		this.productURL = productURL;
+	}
+	/**
+	 * @return the productDescription
+	 */
+	public String getProductDescription() {
+		return productDescription;
+	}
+	/**
+	 * @param productDescription the productDescription to set
+	 */
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	
+	
+}
